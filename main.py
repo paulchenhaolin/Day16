edf4b8e3a6800240e2818b516f51400e2afd0703
paulchenhaolin <54451600+paulchenhaolin@users.noreply.github.com>
2021-08-10 23:07:17 -0500

    Initial commit

--- main.py
+++ main.py
@@ -2,10 +2,17 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import turtle
 
 import another_module
 print(another_module.another_variable)
 
-from turtle import Turtle
+from turtle import Turtle, Screen
 timmy = Turtle()
-print(timmy)
+print(timmy)  # printing an object
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()  # only finish it after you click it
